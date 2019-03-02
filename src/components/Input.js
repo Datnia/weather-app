c104344bc77f6c59f3805aa3fcae6efbdac1d05b
Dat Nguyen <Dat_T_Nguyen@live.com>
2019-03-02 16:55:12 -0500

    first commit

---
+++ src/components/Input.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Input extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <input type="text" />;
+  }
+}
+
+export default Input;
---
+++ src/components/Weather.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Weather extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>maryland</div>;
+  }
+}
+
+export default Weather;
